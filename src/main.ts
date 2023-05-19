4bc6aef37062d72df2fd4a431d6f7a0809e5d599
alexandersus <sasha_suslov07@mail.ru>
2023-05-19 20:14:50 +0300

    feat: implement primary books endpoint

--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,19 @@
-import { NestFactory } from '@nestjs/core';
+import { HttpAdapterHost, NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ConfigService } from '@nestjs/config';
+import { PrismaClientExceptionFilter } from './prisma-client-exception/prisma-client-exception.filter';
+import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   const config = app.get(ConfigService);
   const port = config.get<number>('APP_PORT');
+
+  app.useGlobalPipes(new ValidationPipe());
+
+  const { httpAdapter } = app.get(HttpAdapterHost);
+  app.useGlobalFilters(new PrismaClientExceptionFilter(httpAdapter));
 
   await app.listen(port, () => {
     console.log(`Listening at http://localhost:${port}`);
