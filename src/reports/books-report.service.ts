596be8a309331be18b5b7fc84a494b481014b401
alexandersus <sasha_suslov07@mail.ru>
2023-05-24 18:46:50 +0300

    feat: update response of books report

--- src/reports/books-report.service.ts
+++ src/reports/books-report.service.ts
@@ -1,12 +1,16 @@
 import { Injectable } from '@nestjs/common';
 import { BooksService } from 'src/books/books.service';
+import { ExemplarsService } from 'src/exemplars/exemplars.service';
 
 @Injectable()
 export class BooksReportService {
-  constructor(private readonly booksService: BooksService) {}
+  constructor(
+    private readonly booksService: BooksService,
+    private readonly exemplarsService: ExemplarsService,
+  ) {}
 
   async generateBooksReport() {
-    return this.booksService.findMany({
+    const books = await this.booksService.findMany({
       select: {
         id: true,
         isbn: true,
@@ -16,10 +20,14 @@ export class BooksReportService {
         exemplars: {
           select: {
             id: true,
-            status: true,
           },
         },
       },
     });
+
+    return books.map(({ exemplars, ...restData }) => ({
+      ...restData,
+      totalExemplars: exemplars.length,
+    }));
   }
 }
