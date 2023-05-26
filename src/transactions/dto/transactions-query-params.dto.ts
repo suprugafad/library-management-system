54d1125ffcb3b1a05cb3271e63041fa35058ef9f
alexandersus <sasha_suslov07@mail.ru>
2023-05-26 15:41:20 +0300

    feat: add missed api documenation

--- src/transactions/dto/transactions-query-params.dto.ts
+++ src/transactions/dto/transactions-query-params.dto.ts
@@ -18,19 +18,19 @@ export class TransactionsQueryParamsDto extends PaginationQueryParamsDto {
   @IsOptional()
   exemplarId?: number;
 
-  @ApiProperty({ example: '2023-06-24T22:37:44.231Z' })
+  @ApiProperty({ example: '2023-06-24T22:37:44.231Z', required: false })
   @IsDate()
   @Transform(({ value }) => new Date(value))
   @IsOptional()
   borrowedAt?: Date;
 
-  @ApiProperty({ example: '2023-07-24T22:37:44.231Z' })
+  @ApiProperty({ example: '2023-07-24T22:37:44.231Z', required: false })
   @IsDate()
   @Transform(({ value }) => new Date(value))
   @IsOptional()
   returnedAt?: Date;
 
-  @ApiProperty({ example: '2023-07-24T22:37:44.231Z' })
+  @ApiProperty({ example: '2023-07-24T22:37:44.231Z', required: false })
   @IsDate()
   @Transform(({ value }) => new Date(value))
   @IsOptional()
